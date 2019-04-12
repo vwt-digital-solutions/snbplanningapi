5693faaa4f14b01dc1390cc81048dec95b431d24
Bernie van Veen <b.vanveen@vwt.digital>
2019-04-12 15:21:00 +0200

    Added CORS. CAR-27

--- openapi_server/__init__.py
+++ openapi_server/__init__.py
@@ -1,5 +1,6 @@
 import connexion
 
+from flask_cors import CORS
 from openapi_server import encoder
 
 app = connexion.App(__name__, specification_dir='./openapi/')
@@ -7,3 +8,4 @@ app.app.json_encoder = encoder.JSONEncoder
 app.add_api('openapi.yaml',
             arguments={'title': 'snbplanningtool'},
             pythonic_params=True)
+CORS(app.app)

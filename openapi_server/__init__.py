5b1740ab8feda78359e6cbb7119999c5f625737c
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-04-15 13:20:41 +0200

    Read topic and populate datastore. CAR-32

--- openapi_server/__init__.py
+++ openapi_server/__init__.py
@@ -1,7 +1,10 @@
-import connexion
+from threading import Thread
 
+import connexion
 from flask_cors import CORS
+
 from openapi_server import encoder
+from . import car_locations_db_handler
 
 app = connexion.App(__name__, specification_dir='./openapi/')
 app.app.json_encoder = encoder.JSONEncoder
@@ -9,3 +12,7 @@ app.add_api('openapi.yaml',
             arguments={'title': 'snbplanningtool'},
             pythonic_params=True)
 CORS(app.app)
+
+
+thread = Thread(target=car_locations_db_handler.read_topic)
+thread.start()
---
+++ openapi_server/car_locations_db_handler.py
@@ -0,0 +1,28 @@
+from google.cloud import pubsub_v1, datastore
+from . import config
+import json
+import logging
+
+
+def read_topic():
+    print('Background thread started')
+    client = pubsub_v1.SubscriberClient()
+    subscription = client.subscription_path(config.TOPIC_PROJECT_ID,
+                                            config.SUBSCRIPTION_NAME)
+    db_client = datastore.Client()
+    logging.info('Start pooling car locations')
+    while True:
+        response = client.pull(subscription, 10)
+        for message in response.received_messages:
+            mdata = json.loads(message.message.data)
+            if 'token' in mdata:
+                loc_key = db_client.key('CarLocation', mdata['token'])
+                entity = db_client.get(loc_key)
+                if entity is None:
+                    entity = datastore.Entity(key=loc_key)
+                entity.update({
+                    "geometry": mdata['geometry']
+                })
+                db_client.put(entity)
+                logging.info('Populate location {} - {}'.format(entity.key, entity))
+    pass

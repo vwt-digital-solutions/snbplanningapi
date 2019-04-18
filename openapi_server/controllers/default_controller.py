befdb1676a14df1f861fa85c1b51950cfa3f832a
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2019-04-18 18:05:14 +0200

    Added "offset" parameter to endpoint. CAR-53

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -1,10 +1,11 @@
 import logging
+import datetime
 
 from flask import jsonify
 from google.cloud import datastore
 
 
-def cars_get():  # noqa: E501
+def cars_get(offset):  # noqa: E501
     """Get car locations
 
     Get a list of all car geolocations # noqa: E501
@@ -12,8 +13,12 @@ def cars_get():  # noqa: E501
 
     :rtype: Cars
     """
+
+    offsetDate = datetime.datetime.utcnow() - datetime.timedelta(hours=offset)
+
     db_client = datastore.Client()
     query = db_client.query(kind='CarLocation')
+    query.add_filter('when', '>=', offsetDate)
     query_iter = query.fetch()
     result = {
         "type": "FeatureCollection",

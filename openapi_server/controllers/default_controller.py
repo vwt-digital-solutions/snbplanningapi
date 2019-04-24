12d3c1c4c2d05bfddf725ecb0bcba4504ebce08d
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2019-04-24 13:06:18 +0200

    Added new Cars Info resource to endpoint. CAR-57

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -1,5 +1,6 @@
 import logging
 import datetime
+import random
 
 from flask import jsonify
 from google.cloud import datastore
@@ -17,8 +18,6 @@ def cars_get(offset):  # noqa: E501
     offsetDate = datetime.datetime.utcnow() - datetime.timedelta(hours=offset)
     offsetDate = offsetDate.isoformat()
 
-    print(offsetDate)
-
     db_client = datastore.Client()
     query = db_client.query(kind='CarLocation')
     query.add_filter('when', '>=', offsetDate)
@@ -33,6 +32,42 @@ def cars_get(offset):  # noqa: E501
             "type": "Feature",
             "geometry": entity['geometry'],
             "properties": {}
+        })
+
+    return jsonify(result)
+
+def carsinfo_get(offset):  # noqa: E501
+    """Get car info
+
+    Get a list of all car information # noqa: E501
+
+
+    :rtype: CarsInfo
+    """
+
+    licensePlateItems = ['AA-BB-11', 'CC-DD-22', 'EE-FF-33', 'GG-HH-44', 'II-JJ-55']
+    nameItems = ['Pietje Puk', 'Connie Moeleker', 'Alco Liest', 'Peter Celie', 'Kenny Boeijen']
+
+    offsetDate = datetime.datetime.utcnow() - datetime.timedelta(hours=offset)
+    offsetDate = offsetDate.isoformat()
+
+    db_client = datastore.Client()
+    query = db_client.query(kind='CarLocation')
+    query.add_filter('when', '>=', offsetDate)
+    query_iter = query.fetch()
+    result = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for entity in query_iter:
+        logging.info('{}'.format(entity))
+        result['features'].append({
+            "type": "Feature",
+            "geometry": entity['geometry'],
+            "properties": {
+                "license_plate": random.choice(licensePlateItems),
+                "driver_name": random.choice(nameItems)
+            }
         })
 
     return jsonify(result)
--- openapi_server/models/__init__.py
+++ openapi_server/models/__init__.py
@@ -5,4 +5,5 @@ from __future__ import absolute_import
 # import models into model package
 from openapi_server.models.car import Car
 from openapi_server.models.cars import Cars
+from openapi_server.models.cars_info import CarsInfo
 from openapi_server.models.geometry import Geometry

b392f2a241f9eb4a84201fe2f1f89ce67c11afa0
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-04-29 14:51:50 +0200

    Make id for POST carinfo optional. CAR-64

--- openapi_server/models/car_info.py
+++ openapi_server/models/car_info.py
@@ -75,8 +75,6 @@ class CarInfo(Model):
         :param id: The id of this CarInfo.
         :type id: int
         """
-        if id is None:
-            raise ValueError("Invalid value for `id`, must not be `None`")  # noqa: E501
 
         self._id = id
 

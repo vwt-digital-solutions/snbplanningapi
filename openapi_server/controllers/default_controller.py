8bc02b1214ee4db04eb80b43e605c15b777cf1de
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-04-29 14:27:30 +0200

    Replace property access with dictionary access. CAR-64

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -65,7 +65,7 @@ def carsinfo_post(body):  # noqa: E501
 
     :rtype: CarInfo id
     """
-    logging.info(body)
+    print(body)
     carinfo = body
     entity = None
     db_client = datastore.Client()

99af228df1a8db27a02249c0330dbf88c081b8de
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-04-29 09:30:06 +0200

    Implement GET /tokens{?assigned=true|false}. CAR-63

--- openapi_server/controllers/default_controller.py
+++ openapi_server/controllers/default_controller.py
@@ -86,3 +86,28 @@ def carsinfo_post(body):  # noqa: E501
     """
 
     return make_response('Cars updated', 201)
+
+
+def is_assigned(token, assigned, car_tokens):
+    if assigned is not None:
+        return (assigned and token in car_tokens) or (not assigned and token not in car_tokens)
+    else:
+        return True
+
+
+def list_tokens(assigned):  # noqa: E501
+    """Enumerate tokens
+
+    :rtype array of strings
+    """
+    db_client = datastore.Client()
+    tokens_query = db_client.query(kind='CarLocation')
+    cars_query = db_client.query(kind='CarInfo')
+    car_tokens = []
+    if assigned is not None:
+        car_tokens = [ci['token'] for ci in cars_query.fetch() if ci['token'] is not None and len(ci['token']) > 0]
+
+    tokens = [token.key.id_or_name for token in tokens_query.fetch() if is_assigned(token.key.id_or_name, assigned,
+                                                                                    car_tokens)]
+
+    return make_response(tokens, 201)

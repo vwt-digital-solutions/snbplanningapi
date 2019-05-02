54711371524dfc8b3a50c894c41128376bef867c
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-05-02 16:06:47 +0200

    Model for work item response. CAR-75

--- openapi_server/models/__init__.py
+++ openapi_server/models/__init__.py
@@ -9,3 +9,5 @@ from openapi_server.models.cars import Cars
 from openapi_server.models.cars_info import CarsInfo
 from openapi_server.models.geometry import Geometry
 from openapi_server.models.tokens import Tokens
+from openapi_server.models.work_item import WorkItem
+from openapi_server.models.work_items import WorkItems

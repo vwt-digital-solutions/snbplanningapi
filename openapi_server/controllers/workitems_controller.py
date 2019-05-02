2b2f28fd1fa1398c9f5784fb90bb8b8b7003c1c8
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-05-02 11:09:37 +0200

    Implementation of GET workitems. CAR-75

---
+++ openapi_server/controllers/workitems_controller.py
@@ -0,0 +1,36 @@
+import logging
+import datetime
+import pytz
+
+from flask import jsonify
+from flask import make_response
+from google.cloud import datastore
+
+
+def list_work_items():  # noqa: E501
+    """Get a list of work items
+
+    Get a list of work items # noqa: E501
+
+
+    :rtype: array of work items
+    """
+    db_client = datastore.Client()
+    query = db_client.query(kind='CarLocation')
+    result = [res for res in query.fetch() if
+              res['StartDatumTijd'] < datetime.datetime.now(pytz.utc) < res['EindDatumTijd']]
+    return jsonify(result)
+
+
+def list_all_work_items():  # noqa: E501
+    """Get a list of work items
+
+    Get a list of work items # noqa: E501
+
+
+    :rtype: array of work items
+    """
+    db_client = datastore.Client()
+    query = db_client.query(kind='CarLocation')
+    result = [res for res in query.fetch()]
+    return jsonify(result)

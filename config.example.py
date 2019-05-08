dadf99a3deb82782a9e702a6093c1c0f94f7dce2
Bernie van Veen <b.vanveen@vwt.digital>
2019-05-08 10:43:41 +0200

    Implement oauth2 client credentials flow based on appid claim. CAR-54

--- config.example.py
+++ config.example.py
@@ -1,7 +1,13 @@
 # Uncomment to activate background thread to read messages, specify project and subscription to use
-# TOPIC_PROJECT_ID = 'mytopicsproject'
-# SUBSCRIPTION_NAME = 'mytopicssubscription'
+#  TOPIC_PROJECT_ID = 'mytopicsproject'
+#  SUBSCRIPTION_NAME = 'mytopicssubscription'
 
-OAUTH_EXPECTED_AUDIENCE = 'https://my.expected.audiecne'
+OAUTH_EXPECTED_AUDIENCE = 'https://my.expected.audience'
 OAUTH_EXPECTED_ISSUER = 'https://my.expected.issuer'
-OAUTH_JWKS_URL = 'https://my.expectedd.issuer/well-known/jwks.json'
+OAUTH_JWKS_URL = 'https://my.expected.issuer/well-known/jwks.json'
+
+# Uncomment to activate OAUTH2 client credentials flow based on appid claim
+#  OAUTH_E2E_EXPECTED_AUDIENCE = 'https://my.expected.audience'
+#  OAUTH_E2E_EXPECTED_ISSUER = 'https://my.expected.issuer'
+#  OAUTH_E2E_JWKS_URL = 'https://my.expected.issuer/well-known/jwks.json'
+#  OAUTH_E2E_APPID = 'xxxx'
--- openapi_server/controllers/security_controller_.py
+++ openapi_server/controllers/security_controller_.py
@@ -1,13 +1,19 @@
 import config
+import logging
 from jwkaas import JWKaas
 
 my_jwkaas = None
+my_e2e_jwkaas = None
 
 if hasattr(config, 'OAUTH_JWKS_URL'):
     my_jwkaas = JWKaas(config.OAUTH_EXPECTED_AUDIENCE,
                        config.OAUTH_EXPECTED_ISSUER,
                        jwks_url=config.OAUTH_JWKS_URL)
 
+if hasattr(config, 'OAUTH_E2E_JWKS_URL'):
+    my_e2e_jwkaas = JWKaas(config.OAUTH_E2E_EXPECTED_AUDIENCE,
+                       config.OAUTH_E2E_EXPECTED_ISSUER,
+                       jwks_url=config.OAUTH_E2E_JWKS_URL)
 
 def refine_token_info(token_info):
     if token_info and 'scopes' in token_info:
@@ -28,4 +34,13 @@ def info_from_OAuth2AzureAD(token):
     :return: Decoded token information or None if token is invalid
     :rtype: dict | None
     """
-    return refine_token_info(my_jwkaas.get_connexion_token_info(token))
+    result = my_jwkaas.get_connexion_token_info(token)
+
+    # Check if e2e test token is configured
+    if result is None and my_e2e_jwkaas is not None:
+        token_info = my_e2e_jwkaas.get_connexion_token_info(token)
+        if token_info is not None and 'appid' in token_info and token_info['appid'] == config.OAUTH_E2E_APPID:
+            logging.warning('Approved e2e access token for appid [%s]', token_info['appid'])
+            result = {'scopes': ['snbplanningapi.read'], 'sub': 'e2e'}
+
+    return refine_token_info(result)

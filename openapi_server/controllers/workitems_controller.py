7be76765129045d8c420bc378a233db1ba2bf1c0
Bernie van Veen <b.vanveen@vwt.digital>
2019-08-21 14:44:48 +0200

    Improved workitems resource performance. DAT-603

--- openapi_server/controllers/workitems_controller.py
+++ openapi_server/controllers/workitems_controller.py
@@ -35,7 +35,7 @@ def list_all_work_items():  # noqa: E501
     """
     db_client = datastore.Client()
     query = db_client.query(kind='WorkItem')
-    result = [res for res in query.fetch() if
-              res['status'] in ['Te Plannen', 'Gepland', 'Niet Gereed'] and
-              (not hasattr(config, 'TASK_TYPE_STARTSWITH') or res['task_type'].startswith(config.TASK_TYPE_STARTSWITH))]
+    query.add_filter('task_type', '>=', config.TASK_TYPE_STARTSWITH)
+    result = [res for res in query.fetch() if res['status'] in ['Te Plannen', 'Gepland', 'Niet Gereed']]
+
     return jsonify(result)

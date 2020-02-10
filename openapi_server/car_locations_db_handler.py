e075ad7f689c272838a0de5791c18c0f64d0a3ee
Bernie van Veen <b.vanveen@vwt.digital>
2020-02-10 09:23:33 +0100

    Prepare for batched messages. DAT-1825

--- openapi_server/car_locations_db_handler.py
+++ openapi_server/car_locations_db_handler.py
@@ -22,22 +22,28 @@ def read_topic():
         ack_ids = []
         for message in response.received_messages:
             mdata = json.loads(message.message.data)
-            if 'token' in mdata:
-                loc_key = db_client.key('CarLocation', mdata['token'])
-                entity = db_client.get(loc_key)
-                if entity is None:
-                    entity = datastore.Entity(key=loc_key)
-                if 'when' not in entity or entity['when'] < mdata['when']:
-                    entity.update({
-                        "geometry": mdata['geometry'],
-                        "when": mdata['when']
-                    })
-                    db_client.put(entity)
-                    logging.debug('Populate location {} - {}'.format(entity.key, entity))
-                else:
-                    logging.debug('Skipping {} - late notification {}/{}'
-                                 .format(mdata['token'], mdata['when'], entity['when']))
+            if 'carlocations' in mdata:
+                for carloc in mdata['carlocations']:
+                    update_carlocation(db_client, carloc)
+            elif 'token' in mdata:
+                update_carlocation(db_client, mdata)
             ack_ids.append(message.ack_id)
         if ack_ids:
             client.acknowledge(subscription, ack_ids)
-    pass
+
+
+def update_carlocation(db_client, carloc):
+    loc_key = db_client.key('CarLocation', carloc['token'])
+    entity = db_client.get(loc_key)
+    if entity is None:
+        entity = datastore.Entity(key=loc_key)
+    if 'when' not in entity or entity['when'] < carloc['when']:
+        entity.update({
+            "geometry": carloc['geometry'],
+            "when": carloc['when']
+        })
+        db_client.put(entity)
+        logging.debug('Populate location {} - {}'.format(entity.key, entity))
+    else:
+        logging.debug('Skipping {} - late notification {}/{}'
+                      .format(carloc['token'], carloc['when'], entity['when']))

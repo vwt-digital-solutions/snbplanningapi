5e89349c9df3c61d095d56103d68b21111236420
nicogriffioen <60692751+nicogriffioen@users.noreply.github.com>
2020-02-13 14:03:42 +0100

    Add CloudFunction for adding car locations. - DAT-1884

---
+++ functions/car_locations_consume/dbprocessor/__init__.py
@@ -0,0 +1,37 @@
+from google.cloud import datastore
+import logging
+
+
+class DBProcessor(object):
+    def __init__(self):
+        self.client = datastore.Client()
+        pass
+
+    def process(self, payload):
+        batch = self.client.batch()
+        for car_location in payload['carlocations']:
+            car_location_entity = self.get_car_location(car_location)
+
+            # Check if get_car_location returned an entity.
+            if car_location_entity is not None:
+                batch.put(car_location_entity)
+
+        batch.commit()
+
+    def get_car_location(self, car_location):
+        loc_key = self.client.key('CarLocation', car_location['token'])
+        entity = self.client.get(loc_key)
+        if entity is None:
+            entity = datastore.Entity(key=loc_key)
+        if 'when' not in entity or entity['when'] < car_location['when']:
+            entity.update({
+                "geometry": car_location['geometry'],
+                "when": car_location['when']
+            })
+            return entity
+            logging.debug('Populate location {} - {}'.format(entity.key, entity))
+        else:
+            logging.debug('Skipping {} - late notification {}/{}'
+                          .format(car_location['token'], car_location['when'], entity['when']))
+
+            return None

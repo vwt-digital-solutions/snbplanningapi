a933d95c21775a88fbbfd7f32d4246d67648facd
nicogriffioen <60692751+nicogriffioen@users.noreply.github.com>
2020-02-17 13:05:31 +0100

    Add Cloudfunction for WorkItem subscription - DAT-1887

---
+++ functions/work_items_consume/dbprocessor/__init__.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+
+import pytz
+from google.cloud import datastore
+import logging
+
+
+class DBProcessor(object):
+    def __init__(self):
+        self.client = datastore.Client()
+        pass
+
+    def process(self, payload, gmaps):
+        when = datetime.strptime(payload['when'], '%Y-%m-%dT%H:%M:%S%z')
+
+        work_item = payload['data']
+        work_item_entity = self.get_work_item(work_item, when, gmaps)
+
+        if work_item_entity is not None:
+            self.client.put(work_item_entity)
+
+    def get_work_item(self, work_item, when, gmaps):
+        loc_key = self.client.key('WorkItem', work_item['L2GUID'])
+        entity = self.client.get(loc_key)
+
+        if work_item.get('status') not in ['Te Plannen', 'Gepland', 'Niet Gereed']:
+            # Remove inactive work from DataStore
+            if entity:
+                self.client.delete(loc_key)
+            return None
+        elif not entity or entity['last_updated'] < when:
+            if entity is None:
+                entity = datastore.Entity(key=loc_key)
+            work_item['last_updated'] = when
+            return self.update_db_workitem(entity, gmaps, work_item)
+        else:
+            logging.debug('Skipping {} - late notification {}/{}'
+                          .format(work_item, when, entity['last_updated']))
+
+            return None
+
+    @staticmethod
+    def update_db_workitem(entity, gmaps, work_item):
+        if work_item['end_timestamp']:
+            work_item['end_timestamp'] = datetime.strptime(work_item['end_timestamp'],
+                                                           '%d-%m-%Y %H:%M:%S').astimezone(pytz.utc)
+        if work_item['start_timestamp']:
+            work_item['start_timestamp'] = datetime.strptime(work_item['start_timestamp'],
+                                                             '%d-%m-%Y %H:%M:%S').astimezone(pytz.utc)
+        if gmaps is not None:
+            location = None
+            # Add extra object to the json to check whether it has already tried
+            # to geocode this location
+            # If this object is true, don't geocode
+            if 'isGeocoded' not in entity:
+                entity.update({
+                    "isGeocoded": False
+                })
+            if not entity['isGeocoded']:
+                if 'geometry' not in entity:
+                    if 'zip' in work_item and work_item['zip'] != '':
+                        postcode = ''.join([ch for ch in work_item['zip'] if ch != ' '])
+                        location = gmaps.geocode(postcode)
+                    elif 'city' in work_item and work_item['city'] != '':
+                        address = work_item['city'] + ',Nederlands'
+                        if 'street' in work_item and work_item['street'] != '':
+                            address = work_item['street'] + ',' + address
+                        location = gmaps.geocode(address)
+                if location:
+                    entity.update({
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [
+                                location[0]['geometry']['location']['lng'],
+                                location[0]['geometry']['location']['lat']
+                            ]
+                        }
+                    })
+                entity.update({
+                    "isGeocoded": True
+                })
+
+        entity.update(work_item)
+        logging.debug('Populate work item {} - {}'.format(entity.key, entity))
+        return entity

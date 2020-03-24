d4306a4a5baa9558e84f09e4f87976dcf8a350e0
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-03-24 15:49:18 +0100

    Added non-generated get_token DAT-2368

--- api_server/openapi_server/test/test_default_controller.py
+++ api_server/openapi_server/test/test_default_controller.py
@@ -1,9 +1,31 @@
 # coding: utf-8
 
 from __future__ import absolute_import
+import config
 import unittest
 
+import adal
+
 from openapi_server.test import BaseTestCase
+
+
+def get_token():
+    """
+    Create a token for testing
+    :return:
+    """
+    oauth_expected_authenticator = config.OAUTH_E2E_AUTHORITY_URI
+    client_id = config.OAUTH_E2E_APPID
+    client_secret = config.OAUTH_E2E_CLIENT_SECRET
+    resource = config.OAUTH_E2E_EXPECTED_AUDIENCE
+
+    # get an Azure access token using the adal library
+    context = adal.AuthenticationContext(oauth_expected_authenticator)
+    token_response = context.acquire_token_with_client_credentials(
+        resource, client_id, client_secret)
+
+    access_token = token_response.get('accessToken')
+    return access_token
 
 
 class TestDefaultController(BaseTestCase):
@@ -14,8 +36,10 @@ class TestDefaultController(BaseTestCase):
 
         Get car locations
         """
+        access_token = get_token()
         headers = {
             'Accept': 'application/json',
+            'Authorization': f'Bearer {access_token}',
         }
         response = self.client.open(
             '/cars',

f7695bf247e1f862f23a2b046b8898d77b836898
nicogriffioen <60692751+nicogriffioen@users.noreply.github.com>
2020-05-26 10:37:06 +0200

    Relax penalty constraint, and use correct arguments. DAT-3019

--- functions/planning/constraints/penalty_constraint.py
+++ functions/planning/constraints/penalty_constraint.py
@@ -2,13 +2,13 @@ from ortools.constraint_solver import pywrapcp
 from constraints.constraint import Constraint
 from data.data_model import DataModel
 
-penalty = 40000
+penalty = 800
 
 
 class PenaltyConstraint(Constraint):
-    def apply(self, manager:  pywrapcp.RoutingModel, routing: pywrapcp.RoutingModel, data_model: DataModel):
+    def apply(self, manager:  pywrapcp.RoutingIndexManager, routing: pywrapcp.RoutingModel, data_model: DataModel):
 
-        for node in range(1, data_model.number_of_nodes):
+        for node in range(0, data_model.number_of_nodes):
             routing.AddDisjunction([manager.NodeToIndex(node)], penalty)
 
         return routing

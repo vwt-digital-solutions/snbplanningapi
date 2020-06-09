865cf54524349593c24b776ca8c500c9e805dd2f
nicogriffioen <60692751+nicogriffioen@users.noreply.github.com>
2020-06-09 16:39:44 +0200

    Add consume functions for appointments and shifts. DAT-3203

---
+++ functions/appointments_consume/dbprocessor/__init__.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+import pytz
+from google.cloud import datastore
+import logging
+
+
+class DBProcessor(object):
+    def __init__(self):
+        self.client = datastore.Client()
+        pass
+
+    def process(self, payload):
+        batch = self.client.batch()
+        batch.begin()
+
+        for appointments in payload['appointments']:
+            availability = self.get_availability(appointments)
+
+            # Check if get_car_location returned an entity.
+            if availability is not None:
+                batch.put(availability)
+
+        batch.commit()
+
+
+def get_availability(self, appointment):
+    id = '{0}-{1}'.format(appointment['userId'],
+                          datetime.strptime(appointment['startDate'], '%d-%m-%Y %H:%M:%S').date())
+    key = self.client.key('EmployeeAvailability', id)
+    entity = self.client.get(key)
+
+    if entity is None:
+        entity = datastore.Entity(key=key)
+
+    appointments = entity.get('appointments', [])
+
+    appointments.append({
+        'created_on': parse_date(appointment['createdOn']),
+        'start_date': parse_date(appointment['startDate']),
+        'end_date': parse_date(appointment['endDate']),
+        'description': appointment['description'],
+        'user_id': appointment['userId'],
+        'type_id': appointment['typeId'],
+        'state': appointment['state'],
+        'fullname': appointment['fullname'],
+        'employee_number': appointment['registratienummer'],
+    })
+
+    entity.update({
+        'appointments': appointments,
+    })
+
+    logging.debug('Populate availability with appointment {}'.format(entity.key))
+
+    return entity
+
+
+def parse_date(date_string, local_time_zone=pytz.timezone('Europe/Amsterdam')):
+    """
+    :param date_string: A date string in the format '%d-%m-%Y %H:%M:%S'
+    :param local_time_zone: Override this timezone if the date to parse is in another timezone than Europe/Amsterdam
+    :return: An ISO-formatted date string in UTC.
+    """
+    if not date_string:
+        return ''
+    date = datetime.datetime.strptime(date_string, '%d-%m-%Y %H:%M:%S')
+    date = local_time_zone.localize(date)
+    date = date.astimezone(pytz.utc)
+
+    return date.isoformat()
---
+++ functions/shifts_consume/dbprocessor/__init__.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+import pytz
+from google.cloud import datastore
+import logging
+
+
+class DBProcessor(object):
+    def __init__(self):
+        self.client = datastore.Client()
+        pass
+
+    def process(self, payload):
+        batch = self.client.batch()
+        batch.begin()
+
+        for shift in payload['shifts']:
+            availability = self.get_availability(shift)
+
+            # Check if get_car_location returned an entity.
+            if availability is not None:
+                batch.put(availability)
+
+        batch.commit()
+
+
+def get_availability(self, shift):
+    id = '{0}-{1}'.format(shift['userId'], datetime.strptime(shift['startDate'], '%d-%m-%Y %H:%M:%S').date())
+    key = self.client.key('EmployeeAvailability', id)
+    entity = self.client.get(key)
+
+    if entity is None:
+        entity = datastore.Entity(key=key)
+
+    start_date = parse_date(shift['startDate'])
+    end_date = parse_date(shift['endDate'])
+
+    entity.update({
+        'employee_number': shift['userId'],
+        'shift_date': start_date,
+        'shift_start_date': end_date,
+        'shift_end_date': shift['userShiftEndDate'],
+        'active': shift['isActive'],
+        'name': shift['name'],
+    })
+
+    logging.debug('Populate shift {}'.format(entity.key))
+
+    return entity
+
+
+def parse_date(date_string, local_time_zone=pytz.timezone('Europe/Amsterdam')):
+    """
+    :param date_string: A date string in the format '%d-%m-%Y %H:%M:%S'
+    :param local_time_zone: Override this timezone if the date to parse is in another timezone than Europe/Amsterdam
+    :return: An ISO-formatted date string in UTC.
+    """
+    if not date_string:
+        return ''
+    date = datetime.datetime.strptime(date_string, '%d-%m-%Y %H:%M:%S')
+    date = local_time_zone.localize(date)
+    date = date.astimezone(pytz.utc)
+
+    return date.isoformat()
